4feda3ead3b67fe9c2bfd5de3b7499bd03dbc2fd
jamesodukoya <odukoyaoluwadara@gmail.com>
2024-08-19 11:44:30 +0100

    Update contact page index.html

--- assets/js/index-new.js
+++ assets/js/index-new.js
@@ -1,3 +1,5 @@
+// Built by James Odukoya
+
 gsap.registerPlugin(ScrollTrigger);
 
 let scroll;
@@ -8,6 +10,29 @@ const selectAll = (e) => document.querySelectorAll(e);
 //const container = select('.site-main');
 
 initPageTransitions();
+
+// Viewport Trigger
+        document.addEventListener("DOMContentLoaded", () => {
+
+	// Use Intersection Observer to determine if objects are within the viewport
+	const observer = new IntersectionObserver(entries => {
+	  entries.forEach(entry => {
+		if (entry.isIntersecting) {
+		  entry.target.classList.add('in-view');
+		  return;
+		}
+		entry.target.classList.remove('in-view');
+	  });
+	});
+
+	// Get all the elements with the .reveal class applied
+	const allAnimatedElements = document.querySelectorAll('.reveal');
+
+	// Add the observer to each of those elements
+	allAnimatedElements.forEach((element) => observer.observe(element));
+
+});
+   
 
 // Animation - First Page Load
 function initLoaderHome() { 
