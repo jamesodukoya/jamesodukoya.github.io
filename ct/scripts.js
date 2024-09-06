6bb65932e813ffb66442fa34c331f8a5fcf77c92
jamesodukoya <odukoyaoluwadara@gmail.com>
2024-09-06 09:22:58 +0100

    Complete CT landing page

---
+++ ct/scripts.js
@@ -0,0 +1,39 @@
+const swiperEl = document.querySelector('swiper-container')
+Object.assign(swiperEl, {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+      },
+    });
+swiperEl.initialize();
+    
+document.getElementById("default").click();
+
+function openTab(evt, mountNo) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(mountNo).style.display = "block";
+  evt.currentTarget.className += " active";
+}
